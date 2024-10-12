d0ee457b133c6dba08d112a09207ff09aa13715d
soramico <soramiko.akiyama@gmail.com>
2024-10-12 15:00:24 +0900

    Revert "ProtoSpace挙動確認_修正2"
    This reverts commit ebbc1a0a3393f04be909a82a0cbe309701dce33a.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :cotributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :cotributor_confirmation, only: [ :edit, :update, :destroy]
   
   def index
     @prototypes = Prototype.includes(:user)
@@ -39,21 +39,18 @@ class PrototypesController < ApplicationController
 
   def destroy
     @prototype = Prototype.find(params[:id])
-    if @prototype.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
+    @prototype.destroy
+    redirect_to root_path
   end
 
   private
 
-  def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
-  end
-  
-  def cotributor_confirmation
-    redirect_to root_path unless current_user == @prototype.user
-  end
+def prototype_params
+  params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+end
+
+def cotributor_confirmation
+  redirect_to root_path unless current_user == @prototype.user
+end
 
 end
